9b670887536925feded613b97c016b33ec11a163
Ibrahim Daher <i.daher@raiondigital.com>
2023-05-26 14:59:49 +0300

    all api routes for frontend done

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\QuoteController;
+use App\Http\Controllers\Api\v1\HeaderController;
+use App\Http\Controllers\Api\v1\AboutUsController;
+use App\Http\Controllers\Api\v1\ProjectController;
+use App\Http\Controllers\Api\v1\ServiceController;
+use App\Http\Controllers\Api\v1\ContactUsController;
+use App\Http\Controllers\Api\v1\StaffMemberController;
+use App\Http\Controllers\Api\v1\BriefElementController;
+use App\Http\Controllers\Api\v1\QuoteElementController;
+use App\Http\Controllers\Api\v1\SliderElementController;
+use App\Http\Controllers\Api\v1\ContactSubmissionController;
+use App\Http\Controllers\Api\v1\NewsletterSubscriberController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +26,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 
+/*
+|--------------------------------------------------------------------------
+| React/Frontend Routes
+|--------------------------------------------------------------------------
+|
+|These Routes are intended for the user frontend only. They provide the api routes to use in axios requests from the ReactJs application.
+*/
+
+Route::middleware('guest')->group(function(){
+    Route::get('/about-us',                 [AboutUsController::class, 'index'])          ->name('aboutElements.index');
+    Route::get('/brief',                    [BriefElementController::class, 'index'])     ->name('briefElements.index');
+    Route::post('/submit-contact',          [ContactSubmissionController::class, 'store'])      ->name('contact.store');
+    Route::get('/contact-us',               [ContactUsController::class, 'index'])      ->name('contactElements.index');
+    Route::get('/header',                   [HeaderController::class, 'index'])          ->name('headerElements.index');
+    Route::post('/subscribe-to-newsletter', [NewsletterSubscriberController::class, 'store'])->name('newsletter.store');
+    Route::get('/projects',                 [ProjectController::class, 'index'])        ->name('projectElements.index');
+    Route::post('/quote-submisson',         [QuoteController::class, 'store'])                    ->name('quote.store');
+    Route::get('/quote-section',            [QuoteElementController::class, 'index'])     ->name('quoteElements.index');
+    Route::get('/services',                 [ServiceController::class, 'index'])        ->name('serviceElements.index');
+    Route::get('/sliders',                  [SliderElementController::class, 'index'])   ->name('sliderElements.index');
+    Route::get('/staff-members',            [StaffMemberController::class, 'index'])       ->name('staffMembers.index');
+});
